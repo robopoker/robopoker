9bf0e69871f1c62eb1d3a364e2e499928e7434da
robopoker <robopoker@robopoker.org>
2012-03-28 16:49:06 +0400

    version changed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import py2exe
 
 setup(
     name='robopoker-sdk-src',
-    version='0.25',
+    version='0.26',
     description='Poker engine and bot development kit',
     url='http://robopoker.org',
     download_url='http://robopoker.org.vbo.dev.vbo.name/about/sdk/',
